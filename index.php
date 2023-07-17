2d2326125ef9c03341b0b2ca4a71da9a569376c6
xhao016 <xinhao1016@gmail.com>
2023-07-18 01:04:50 +0800

    prevent select future date

--- index.php
+++ index.php
@@ -122,6 +122,30 @@ if(isset($_POST['submit'])){
     });
   }, false);
 })();
+
+
+const resetButton = document.querySelector('#reset');
+ const inputFields = document.querySelectorAll('input');
+
+ resetButton.addEventListener('click', () => {
+ inputFields.forEach((input) => {
+     input.value = '';
+ });
+ });
+
+//Display Only Date till today //
+
+var dtToday = new Date();
+var month = dtToday.getMonth() + 1;     // getMonth() is zero-based
+var day = dtToday.getDate();
+var year = dtToday.getFullYear();
+if(month < 10)
+   month = '0' + month.toString();
+if(day < 10)
+   day = '0' + day.toString();
+
+var maxDate = year + '-' + month + '-' + day;
+$('#month').attr('max', maxDate);
 </script>
 
 
