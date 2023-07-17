611fb9cc770778d5a6bf4da40ad6c1af91dc1dbc
xhao016 <xinhao1016@gmail.com>
2023-07-17 14:50:47 +0800

    delete

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'connect.php';
+
+if(isset($_GET['deleteid'])){
+    $record_id = mysqli_real_escape_string($con, $_GET['deleteid']);
+
+    // Delete patient information from the database
+    $sql = "DELETE FROM record_account WHERE record_id = $record_id";
+    $result = mysqli_query($con, $sql);
+
+    if($result){
+        header('location: salary.php?deleted=true'); // Redirect to salary.php with a parameter
+    }else{
+        die(mysqli_error($con));
+    }
+    
+}
+
+?>
