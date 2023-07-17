05a34ef472c836366c3b7b13c5bc14ceb30bf9b3
xhao016 <xinhao1016@gmail.com>
2023-07-18 00:58:15 +0800

    fix delete foreign key table first

--- delete.php
+++ delete.php
@@ -6,9 +6,14 @@ if(isset($_GET['deleteid'])){
     $record_id = mysqli_real_escape_string($con, $_GET['deleteid']);
 
     // Delete patient information from the database
+
+    
+    $sql = "DELETE FROM investment_type WHERE record_id = $record_id";
+    mysqli_query($con, $sql);
+
+    
     $sql = "DELETE FROM record_account WHERE record_id = $record_id";
     $result = mysqli_query($con, $sql);
-
     if($result){
         header('location: salary.php?deleted=true'); // Redirect to salary.php with a parameter
     }else{
