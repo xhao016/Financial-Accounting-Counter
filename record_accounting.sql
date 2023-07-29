8bcef24ab783df18585215d83183cee98f01c361
xhao016 <xinhao1016@gmail.com>
2023-07-29 16:05:05 +0800

    update

--- record_accounting.sql
+++ record_accounting.sql
@@ -32,13 +32,14 @@ CREATE TABLE IF NOT EXISTS `investment_type` (
   PRIMARY KEY (`investment_id`),
   KEY `record_id` (`record_id`),
   CONSTRAINT `investment_type_ibfk_1` FOREIGN KEY (`record_id`) REFERENCES `record_account` (`record_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table record_accounting.investment_type: ~0 rows (approximately)
-REPLACE INTO `investment_type` (`investment_id`, `record_id`, `necessity_account`, `financial_freedom`, `education_account`, `long_term_save`, `entertainment`, `give_account`) VALUES
-	(1, 2, 825.00, 150.00, 150.00, 150.00, 150.00, 75.00),
-	(2, 3, 8250.00, 1500.00, 1500.00, 1500.00, 1500.00, 750.00),
-	(3, 4, 8250.00, 1500.00, 1500.00, 1500.00, 1500.00, 750.00);
+-- Dumping data for table record_accounting.investment_type: ~4 rows (approximately)
+INSERT INTO `investment_type` (`investment_id`, `record_id`, `necessity_account`, `financial_freedom`, `education_account`, `long_term_save`, `entertainment`, `give_account`) VALUES
+	(3, 4, 8250.00, 1500.00, 1500.00, 1500.00, 1500.00, 750.00),
+	(5, 6, 1925.00, 350.00, 350.00, 350.00, 350.00, 175.00),
+	(7, 8, 2750.00, 500.00, 500.00, 500.00, 500.00, 250.00),
+	(8, 9, 6789.75, 1234.50, 1234.50, 1234.50, 1234.50, 617.25);
 
 -- Dumping structure for table record_accounting.record_account
 CREATE TABLE IF NOT EXISTS `record_account` (
@@ -46,14 +47,13 @@ CREATE TABLE IF NOT EXISTS `record_account` (
   `date_record` varchar(50) DEFAULT NULL,
   `amount_income` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`record_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table record_accounting.record_account: ~0 rows (approximately)
-REPLACE INTO `record_account` (`record_id`, `date_record`, `amount_income`) VALUES
-	(1, '2023-07-05', 1500.00),
-	(2, '2023-07-05', 1500.00),
-	(3, '2023-06-29', 15000.00),
-	(4, '2023-06-29', 15000.00);
+-- Dumping data for table record_accounting.record_account: ~3 rows (approximately)
+INSERT INTO `record_account` (`record_id`, `date_record`, `amount_income`) VALUES
+	(6, '2023-07-19', 3500.00),
+	(8, '2023-07-20', 5000.00),
+	(9, '2023-07-31', 12345.00);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
